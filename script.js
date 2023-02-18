4964e7761a73d7711f668a105b6210615cbb7a70
Kuba <67916528+Kubaplay07@users.noreply.github.com>
2023-02-18 17:34:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const copyEl = document.getElementById('copy');
+const passwordEl = document.querySelector('.password-display');
+
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>/?';
+
+function generatePassword() {
+  let password = '';
+  const characterList = [];
+  if (uppercaseEl.checked) {
+    characterList.push(...uppercaseLetters);
+  }
+  if (lowercaseEl.checked) {
+    characterList.push(...lowercaseLetters);
+  }
+  if (numbersEl.checked) {
+    characterList.push(...numbers);
+  }
+  if (symbolsEl.checked) {
+    characterList.push(...symbols);
+  }
+  const maxLength = Math.min(lengthEl.value, 100);
+  for (let i = 0; i < maxLength; i++) {
+    password += characterList[Math.floor(Math.random() * characterList.length)];
+  }
+  if (characterList.length === 0) {
+    password = '';
+  }
+  return password;
+}
+
+function updatePassword() {
+  const password = generatePassword();
+  passwordEl.innerText = password;
+}
+
+function onLoad() {
+  updatePassword();
+}
+
+generateEl.addEventListener('click', () => {
+  updatePassword();
+});
+
+lengthEl.addEventListener('input', () => {
+  lengthEl.value = Math.min(lengthEl.value, 100); // limit max length to 100
+  updatePassword();
+});
+
+uppercaseEl.addEventListener('change', () => {
+  updatePassword();
+});
+
+lowercaseEl.addEventListener('change', () => {
+  updatePassword();
+});
+
+numbersEl.addEventListener('change', () => {
+  updatePassword();
+});
+
+symbolsEl.addEventListener('change', () => {
+  updatePassword();
+});
+
+
+
+
+copyEl.addEventListener('click', () => {
+    const password = passwordEl.innerText;
+    if (password) {
+      navigator.clipboard.writeText(password);
+      copyEl.innerText = 'Password Copied!';
+      setTimeout(() => {
+        copyEl.innerText = 'Copy Password';
+      }, 2000);
+    }
+  });
